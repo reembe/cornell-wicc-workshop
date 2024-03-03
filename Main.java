0d99daa282fcfe4eb13c96c4882bb912305d7170
Reem Belafkih <130573894+reembe@users.noreply.github.com>
2024-03-03 13:47:55 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
-        if (input.equals("Has a stripe"))
-        {
-            System.out.println("Viceroy");
-        }
-        else if(input.equals("Does not have a stripe"))
-        {
-            System.out.println("Monarch");
-        }
-
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String input = scan.nextLine();
+    String[] arr = input.split(" ");
+    int x1 = Integer.parseInt(arr[0]);
+    int y1 = Integer.parseInt(arr[1]);
+    int x2 = Integer.parseInt(arr[2]);
+    int y2 = Integer.parseInt(arr[3]);
+    if ((x1 == x2 || y1 == y2 || Math.abs(x1 - x2) == Math.abs(y1 - y2)) || (x1 +1 == x2 && y1 +2 == y2 || x1+2 == x2 && y1 +1 == y2)){
+        System.out.println("YES");
+    } else {
+        System.out.println("NO");
     }
+
+  }
 }
+
+      
